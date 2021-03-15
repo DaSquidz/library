6c238c7e7403c732bbfe97643548a7675c4df578
Riccardo Geri <geririccardo34@gmail.com>
2021-03-15 18:46:30 +0100

    Start working on the books grid

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 'use strict';
 
 let myLibrary = [];
+const bookGrid = document.getElementById("book-grid");
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {return `${title} by ${author}, ${pages} pages, ${read}`};
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    newBook = newBook(title, author, pages, read);
+    let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
+function populateGrid() {
+    let bookCard = document.createElement("div");
+    let title = document.createElement("h2");
+    let author = document.createElement("p");
+    let pages = document.createElement("p");
+    let read = document.createElement("button");
+
+    
+}
+
+addBookToLibrary("Inferno", "Dante", 562, true);
+
+addBookToLibrary("1984", "George Orwell", 385, true);
+
+addBookToLibrary("Il mistero di Babbo Natale", "Abbefana", 43, false);
+
