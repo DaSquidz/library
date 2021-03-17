3cb4c789b0183750f94423350d7937d166f5c342
Riccardo Geri <geririccardo34@gmail.com>
2021-03-17 23:44:49 +0100

    Add functionality to Add Book button, still to finish

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Library app that lets you track read books.
+The goal of this project is to practice with objects and object constructors. I still haven't learned about factory functions and module patterns so most variables will be in global scope.
+
 Live demo: https://dasquidz.github.io/library/
--- script.js
+++ script.js
@@ -5,18 +5,27 @@ let myLibrary = [];
 const bookGrid =  document.getElementById('book-grid');
 const form = document.getElementById('form');
 const newBookBtn = document.getElementById('new-book');
-const closeFormBtn = document.getElementsByClassName("close")[0];
+const closeFormBtn = document.getElementById('close-btn');
+
+const formTitle =  document.getElementById('form-title');
+const formAuthor =  document.getElementById('form-author');
+const formPages =  document.getElementById('form-pages');
+const formRead =  document.getElementById('form-read');
+const addBookBtn =  document.getElementById('add-book');
+const formButtons = [formTitle, formAuthor, formPages, formRead];
 
 newBookBtn.addEventListener('click', () => form.style.display = "block");
 closeFormBtn.addEventListener('click', () => form.style.display = "none");
 window.addEventListener('click', (event) => {if(event.target == form)form.style.display = "none"});
 
 function Book(title, author, pages, read) {
+    let readValue = read === true ? 'Already read' : 'Not read yet';
+
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {return `${title} by ${author} ${pages} pages, ${read}`};
+    this.info = function() {return `${title} by ${author} ${pages} pages, ${readValue}`};
 };
 
 function addBookToLibrary(title, author, pages, read) {
@@ -25,8 +34,8 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 //2 books already defined for testing
-addBookToLibrary('Inferno', 'Dante', 656, 'Already read');
-addBookToLibrary('Paradiso', 'Virgil', 295, 'Not read');
+addBookToLibrary('Inferno', 'Dante', 656, true);
+addBookToLibrary('Paradiso', 'Virgil', 295, false);
 let Inferno = myLibrary[0];
 
 //clear the grid first,and then populate to avoid duplicates
@@ -47,6 +56,7 @@ function clearGrid(){
   });
 }
 
-function clearForm() {
-  
-}
+addBookBtn.addEventListener('click', function() {
+    addBookToLibrary(formTitle.value, formAuthor.value, formPages.value, formRead.checked)
+    formButtons.forEach(() => this.value = "");
+});
