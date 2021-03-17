3e94d78522778a988ef29c292a8a2361bc3a34be
Riccardo Geri <geririccardo34@gmail.com>
2021-03-18 00:55:30 +0100

    Add functioning remove button

--- script.js
+++ script.js
@@ -2,54 +2,68 @@
 
 let myLibrary = [];
 
-const bookGrid =  document.getElementById('book-grid');
-const form = document.getElementById('form');
-const newBookBtn = document.getElementById('new-book');
-const closeFormBtn = document.getElementById('close-btn');
+const bookGrid      = document.getElementById('book-grid');
+const form          = document.getElementById('form');
+const newBookBtn    = document.getElementById('new-book');
+const closeFormBtn  = document.getElementById('close-btn');
+const formFields    = document.getElementById('form-fields');
+const formTitle     = document.getElementById('form-title');
+const formAuthor    = document.getElementById('form-author');
+const formPages     = document.getElementById('form-pages');
+const formRead      = document.getElementById('form-read');
+const addBookBtn    = document.getElementById('add-book');
+let removeButtons   = document.getElementsByClassName('remove-btn');
 
-const formFields = document.getElementById('form-fields');
-const formTitle =  document.getElementById('form-title');
-const formAuthor =  document.getElementById('form-author');
-const formPages =  document.getElementById('form-pages');
-const formRead =  document.getElementById('form-read');
-const addBookBtn =  document.getElementById('add-book');
-
-newBookBtn.addEventListener('click', () => form.style.display = "block");
-closeFormBtn.addEventListener('click', () => form.style.display = "none");
-window.addEventListener('click', (event) => {if(event.target == form)form.style.display = "none"});
+newBookBtn.addEventListener('click', () =>      form.style.display = "block");
+closeFormBtn.addEventListener('click', () =>    form.style.display = "none");
+window.addEventListener('click', (event) =>     {if(event.target == form)form.style.display = "none"});
 
 function Book(title, author, pages, read) {
     let readValue = read === true ? 'Already read' : 'Not read yet';
 
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {return `${title} by ${author} ${pages} pages, ${readValue}`};
+    this.title      = title;
+    this.author     = author;
+    this.pages      = pages;
+    this.read       = read;
+    this.info       = function() {return `${title} by ${author} ${pages} pages, ${readValue}`};
 };
-
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
-}
-
+};
 function populateGrid(){
   clearGrid();
   for(let i = 0; i < myLibrary.length; i++){
-    let bookInfo = document.createElement('div');
-    bookInfo.className = 'book';
-    bookInfo.textContent = myLibrary[i].info();
-    
-    bookGrid.appendChild(bookInfo);
-  }
-}
+    const bookCard = document.createElement('div');
+    const bookInfo = document.createElement('div');
+    const cardFunctions = document.createElement('div');
+    const removeButton = document.createElement('button');
 
+    bookCard.className = 'book';
+    bookInfo.className = 'book-info';
+    cardFunctions.className = 'card-func';
+    removeButton.className = 'remove-btn';
+
+    removeButton.value = i;
+    removeButton.addEventListener('click', function() {
+        removeBook(removeButton.value);
+    });
+
+    bookInfo.textContent = myLibrary[i].info();
+    removeButton.innerText = 'Remove';
+    
+    cardFunctions.appendChild(removeButton);
+    bookCard.appendChild(bookInfo);
+    bookCard.appendChild(cardFunctions);
+
+    bookGrid.appendChild(bookCard);
+  }
+};
 function clearGrid(){
   document.querySelectorAll('.book').forEach(function(book){
         book.remove();
   });
-}
-
+};
 addBookBtn.addEventListener('click', function() {
     if(formTitle.value !== "") {
         if(formAuthor.value !== ""){ 
@@ -65,3 +79,9 @@ addBookBtn.addEventListener('click', function() {
     }
     else alert("Invalid title");
 });
+
+function removeBook(value) {
+    myLibrary.splice(value, 1);
+    populateGrid();
+}
+//removeButtons.addEventListener('click', removeBook(removeButtons.indexOf(this)))
